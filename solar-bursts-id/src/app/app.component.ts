be58c1a103c9afb63e9e0597a04a9d7cc3431c55
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-03-06 11:05:55 +0530

    Initialized ng app, added apex, material

---
+++ solar-bursts-id/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component,ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+export type ChartOptions = {
+  series:ApexAxisChartSeries;
+  chart:ApexChart;
+  xaxis:ApexXAxis;
+  title:ApexTitleSubtitle;
+};
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  @ViewChild("chart") chart!:ChartComponent;
+  public chartOptions!:Partial<ChartOptions>;
+  constructor(){
+    this.chartOptions = {
+      series:[
+        {
+          name:"My-series",
+          data:[10,41,35,51,49,62,69,91,148]
+        }
+      ],
+      chart:{
+        height:350,
+        type:"bar"
+      },
+      title:{
+        text:"MyFAC"
+      },
+      xaxis:{
+        categories:["Jan","Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+      }
+    }
+  }
+  title = 'solar-bursts-id';
+}
---
+++ solar-bursts-id/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {NgApexchartsModule} from 'ng-apexcharts';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    NgApexchartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
