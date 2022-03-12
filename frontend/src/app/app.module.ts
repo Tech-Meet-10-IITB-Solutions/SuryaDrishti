c337640ff7fad91a12fb771f441ed47babe490b2
devansh-dvj <devansh.dvj@gmail.com>
2022-03-12 17:53:24 +0530

    Added base of frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { ReportComponent } from './report/report.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    ReportComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
